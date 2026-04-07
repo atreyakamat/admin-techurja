dd6fdb99d297e04e33e4037c285c490d1ea0082f
Atreya Kamat <atkamat1204@gmail.com>
2026-04-07 10:23:27 +0530

    Restructured: Moved Next.js application to root, moved PHP code to php-instance branch

--- routes/console.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Artisan;
-
-Artisan::command('inspire', function () {
-    $this->comment('Techurja Arena Master Platform Online.');
-})->purpose('Display an inspiring message');
